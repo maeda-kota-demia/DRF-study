839edcc12958574e3cf604c364e2f70b4bc28d31
maeda-kota-demia <kota_maeda@demia.co.jp>
2026-05-25 14:10:05 +0900

    first-commit

--- api/models.py
+++ api/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+# api/models.py
+from django.contrib.auth.models import AbstractUser
+class User(AbstractUser):
+    pass
+
+class Todo(models.Model):
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='todos'
+    )
+    title = models.CharField(max_length=100)
+    is_done = models.BooleanField()
+
+    def __str__(self):
+        return self.title
--- api/views.py
+++ api/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .models import Todo
+from .serializers import TodoSerializer
 
 # Create your views here.
+
+class TodoListView(APIView):
+    def get(self,request,*args,**kwargs):
+        queryset = Todo.objects.all()
+        serializer = TodoSerializer(queryset,many=True)
+
+        return Response(serializer.data)
+    
+    def post(self,request,*args,**kwargs):
+        serializer = TodoSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data,status=201)
